97db1e9a5a14cd30e072ffbd0a61cd954368e00d
LAURA FURTADO DOS REIS <202402630989@alunos.ibmec.edu.br>
2025-02-17 08:28:19 -0300

    first commit

--- src/AC3/AC3.java
+++ src/AC3/AC3.java
@@ -1,9 +1,25 @@
 package AC3;
 
+import java.util.Scanner;
+
 public class AC3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		double area, volume, h;
+		int raio;
+		System.out.println("Digite o raio do cilindro: ");
+		raio = teclado.nextInt();
+		System.out.println("Insira a altura do cilindro: ");
+		teclado.nextLine();
+		h = teclado.nextDouble();
+		area = 2*Math.PI*raio*(raio+h);
+		volume = Math.PI*raio*raio*h;
+		System.out.println("A altura do cilindro é "+ h);
+		System.out.println("O raio do cilindro é: "+ raio);
+		System.out.println("A área do cilindro é: "+ area);
+		System.out.println("O volume do cilindro é: "+ volume);
+		
 
 	}
 
