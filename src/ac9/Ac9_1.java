fce37f4be3047d1586c6d8229d61d54e4313f960
laura-furtado <202402630989@alunos.ibmec.edu.br>
2025-02-28 09:02:56 -0300

    AC9

--- src/ac9/Ac9_1.java
+++ src/ac9/Ac9_1.java
@@ -1,5 +1,24 @@
 package ac9;
 
+import java.util.Scanner;
+
 public class Ac9_1 {
 
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		Cilindro c1 = new Cilindro();
+		System.out.println("Digite a altura: ");
+		c1.altura = teclado.nextDouble();
+		System.out.println("Digite o raio: ");
+		c1.raio = teclado.nextDouble();
+		
+		c1.calcularArea(c1.raio, c1.altura);
+		c1.calcularVolume(c1.raio, c1.altura);
+		c1.exibirDados();
+	}
 }
+
+		
+		
+
--- src/ac9/Cilindro.java
+++ src/ac9/Cilindro.java
@@ -10,8 +10,15 @@ public class Cilindro {
 	public void calcularArea(double raio, double altura) {
 		area = raio*raio*Math.PI + altura*(2*Math.PI*raio);
 	}
+	//acesso tipo de retorno nome (parâmetro 1, parâmentro 2)
 	public void calcularVolume(double raio, double altura) {
 		volume = Math.PI*raio*raio*altura;
 	}
 	
+	public void exibirDados() {
+		System.out.println("O raio do cilindro é "+ raio);
+		System.out.println("A altura do cilindro é "+ altura);
+		System.out.printf("A área do cilindro é %.2f\n", area);
+		System.out.printf("O volume do cilindro é %.2f", volume);
+	}
 	}
